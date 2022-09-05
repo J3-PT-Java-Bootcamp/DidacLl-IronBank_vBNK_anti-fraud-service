742d884223a3c84b4458d829fb24a7fe49cd0b1f
VladScv <ddcllrnsbrv@uoc.edu>
2022-09-05 02:46:49 +0200

    BASIC_ARCH: all services created, and develop structure under vBNK_sys

---
+++ src/main/java/com/ironhack/vbnk_antifraudservice/VBnkAntiFraudServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.vbnk_antifraudservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VBnkAntiFraudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VBnkAntiFraudServiceApplication.class, args);
+	}
+
+}
