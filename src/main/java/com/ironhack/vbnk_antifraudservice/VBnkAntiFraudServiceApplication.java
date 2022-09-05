47c845406ba30951c9a1f3b517c75d4ce6693400
VladScv <ddcllrnsbrv@uoc.edu>
2022-09-05 22:44:13 +0200

    configure connexions

--- src/main/java/com/ironhack/vbnk_antifraudservice/VBnkAntiFraudServiceApplication.java
+++ src/main/java/com/ironhack/vbnk_antifraudservice/VBnkAntiFraudServiceApplication.java
@@ -2,8 +2,11 @@ package com.ironhack.vbnk_antifraudservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class VBnkAntiFraudServiceApplication {
 
 	public static void main(String[] args) {
